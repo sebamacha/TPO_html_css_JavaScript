3de1d4fbf8be4355c56f9de0612b4a6b74b8bd90
“sebamacha” <sebastian.j.macharette@gmail.com>
2023-05-03 15:18:45 -0300

    3_4_2023_2

--- js/js.js
+++ js/js.js
@@ -1,11 +1,20 @@
-$(document).ready(function() {
+// Seleccionamos el header y el botón de menú
+const header = document.querySelector('header');
+const menuBtn = document.querySelector('.menu-btn');
 
-	$(window).on('load scroll resize', function() {
-		if( $(this).scrollTop() > 0 ) {
-			$('header').addClass('header2');	
-		} else {
-				$('header').removeClass('header2');	
-			}
-	});
+// Añadimos un evento al botón de menú para mostrar/ocultar el menú
+menuBtn.addEventListener('click', () => {
+  header.classList.toggle('show');
+});
 
+// Añadimos un evento al hacer scroll en la ventana
+window.addEventListener('scroll', () => {
+  // Si la posición vertical del scroll es mayor o igual a la altura del header
+  if (window.pageYOffset >= header.offsetHeight) {
+    // Añadimos la clase 'sticky' al header
+    header.classList.add('sticky');
+  } else {
+    // En caso contrario, eliminamos la clase 'sticky'
+    header.classList.remove('sticky');
+  }
 });
